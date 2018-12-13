e68309822a650dc1f0ea027691d39e5f351adedf
LABCAT <shane.watters.nz@gmail.com>
2018-12-14 11:13:03 +1300

    Adding support for wide images.

--- functions.php
+++ functions.php
@@ -1,11 +1,12 @@
 <?php
 //include any classes needed for the theme
-require_once('classes/style-and-script-controller.php');
-require_once('classes/theme-customizer.php');
-require_once('classes/theme-wrapper.php');
+require_once( 'classes/style-and-script-controller.php' );
+require_once( 'classes/theme-customizer.php' );
+require_once( 'classes/theme-wrapper.php' );
 
-//support post-thumbnails/featured images
-add_theme_support('post-thumbnails');
+//add theme support for various features
+add_theme_support( 'post-thumbnails' );
+add_theme_support( 'align-wide' );
 
 //hide the admin toolbar from the front end
 show_admin_bar( false );
