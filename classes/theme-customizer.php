43f9a951d6da7e5af014d8322276ccecc822d6fc
LABCAT <shane.watters.nz@gmail.com>
2018-12-18 08:44:31 +1300

    Adding default (blank) option to font choices in the customizer.

--- classes/theme-customizer.php
+++ classes/theme-customizer.php
@@ -12,6 +12,7 @@ class ThemeCustomizer {
     //this is also interesting and worth considering:
     //https://inkbotdesign.com/google-font-combinations-mixing-typefaces/
     private $font_choices = [
+		'' => '',
 		'Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i' => 'Roboto',
         'Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i' => 'Open Sans',
         'Lato:100,100i,300,300i,400,400i,700,700i,900,900i' => 'Lato',
