c93be7d4744e83444da89247c4965302abcdfe7b
LABCAT <shane.watters.nz@gmail.com>
2019-01-20 19:48:26 +1300

    Developing 404 page.

---
+++ 404.php
@@ -0,0 +1,20 @@
+<div class="container">
+    <div class="error-404 not-found">
+
+        <header class="entry-header">
+            <h1 class="entry-title">
+                Page not found.
+            </h1>
+    	</header>
+
+    	<div class="entry-content">
+            <p>
+                Sorry, the URL you are looking for doesn’t exist. Maybe you can find what you are looking for in the menu below.
+            </p>
+    		<?php
+    		      ThemeMenus::main_menu( 'page-not-found-nav' );
+    		?>
+    	</div>
+
+    </div>
+</div>
