aeea9b457108d1706dd61579cfd067c06c011bf3
LABCAT <shane.watters.nz@gmail.com>
2019-01-30 16:19:26 +1300

    Completing masonry dev.

---
+++ _dev/src/js/components/menu-masonry.js
@@ -0,0 +1,83 @@
+var menuMasonry = {
+
+    menuContainer: jQuery('.menu-container'),
+
+    columns: jQuery('.wp-block-column'),
+
+    menuSectionClass: 'menu-section',
+
+    menuItemsClass: 'wp-block-dining-dashboard-menu-item',
+
+    menuItems: jQuery('.wp-block-dining-dashboard-menu-item'),
+
+    init: function() {
+        if(this.menuContainer.length){
+            this.setupMenuSections();
+            this.windowResizeListener();
+            this.activateMasonry();
+        }
+    },
+
+    activateMasonry: function() {
+        //reset top margin on menu items
+        this.menuItems.css('marginTop', 0);
+
+        if(! this.isSingleColumn()){
+
+            let columnMargin = parseInt(this.columns.css('marginBottom'));
+
+            jQuery('.' + menuMasonry.menuSectionClass).each(
+                function(i, el){
+                    let previousItem = {};
+                    let currentItem = {};
+                    jQuery(el).find('.' + menuMasonry.menuItemsClass).each(
+                        function(i, el){
+                            if(i % 2){
+                                if(! jQuery.isEmptyObject(previousItem)){
+                                    currentItem = jQuery(el);
+                                    let prevBottom = previousItem.offset().top + previousItem.outerHeight();
+                                    let currentTop = currentItem.offset().top;
+                                    let distance = currentTop - prevBottom - columnMargin;
+                                    currentItem.css('marginTop', -distance);
+                                    previousItem = currentItem;
+                                }
+                                else {
+                                    previousItem = jQuery(el);
+                                }
+                            }
+                        }
+                    );
+                }
+            );
+        }
+    },
+
+    windowResizeListener: function() {
+        jQuery(window).on(
+            'resize',
+            function(){
+                menuMasonry.activateMasonry()
+            }
+        );
+    },
+
+    setupMenuSections: function() {
+        let menuSections = jQuery('.wp-block-columns').find('h2').closest('.wp-block-columns');
+        menuSections.addClass('menu-section-heading');
+
+        menuSections.each(
+            function(i, el){
+                 let contents = jQuery(el).nextUntil('.menu-section-heading');
+                 let section = jQuery('<section>').insertAfter(el);
+                 section.addClass(menuMasonry.menuSectionClass);
+                 section.append(contents);
+            }
+        );
+    },
+
+    isSingleColumn: function() {
+        return this.columns.width() == this.columns.parent().width();
+    }
+}
+
+module.exports = menuMasonry;
