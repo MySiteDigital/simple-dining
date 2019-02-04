75537706d2bc089dc66f4303cfcfb2197c312fbd
LABCAT <shane.watters.nz@gmail.com>
2019-02-05 09:33:11 +1300

    Adding HTML directly to menu-page template to make testing and further design easier.

--- menu-page.php
+++ menu-page.php
@@ -4,15 +4,115 @@
  */
  ?>
 
- <div class="container menu-container">
-     <?php
-         if ( have_posts() ) {
+<div class="container menu-container">
+    <?php
+        //this template is hardcoded for now for testing purposes
+        // if ( have_posts() ) {
+        //
+        //     while ( have_posts() ) {
+        //          the_post();
+        //
+        //          get_template_part( 'template-parts/content/content', 'page' );
+        //     }
+        // }
+    ?>
+    <article id="post-103" class="post-103 page type-page status-publish hentry">
 
-             while ( have_posts() ) {
-                  the_post();
+        <header class="entry-header">
+            <h1 class="entry-title">Menu</h1>
+        </header>
 
-                  get_template_part( 'template-parts/content/content', 'page' );
-             }
-         }
-     ?>
- </div>
+        <div class="entry-content">
+
+            <div class="wp-block-columns has-2-columns">
+                <div class="wp-block-column">
+                    <p>Cake tootsie roll carrot cake sugar plum. Dragée sesame snaps caramels  chocolate bar cotton candy oat cake. Carrot cake cupcake marzipan jelly  beans dessert macaroon muffin. Topping chocolate cake gummi bears  marzipan tootsie roll marzipan candy. Gingerbread candy canes apple pie  jujubes soufflé chocolate bar topping apple pie dragée. Jelly beans  macaroon biscuit. </p>
+                    <h2>Breakfast</h2>
+                </div>
+                <div class="wp-block-column">
+                    <p></p>
+                </div>
+            </div>
+            <div class="wp-block-columns has-2-columns">
+                <div class="wp-block-column">
+                    <figure class="wp-block-dining-dashboard-menu-item">
+                        <header>
+                            <h3>Fresh Bircher Muesli</h3>
+                            <span class="price">$10.00</span>
+                        </header>
+                        <div class="description">
+                            <img src="/wp-content/uploads/2019/02/dose-juice-1184446-unsplash.jpg">
+                            <p>Soaked overnight in Apple juice and manuka honey, served with fresh kiwifruit and strawberries.</p>
+                        </div>
+                    </figure>
+                </div>
+                <div class="wp-block-column">
+                    <figure class="wp-block-dining-dashboard-menu-item">
+                        <header>
+                            <h3>Avocado on Toast</h3>
+                            <span class="price">$12.00</span>
+                        </header>
+                        <div class="description">
+                            <img src="/wp-content/uploads/2019/02/wesual-click-1136560-unsplash.jpg" alt="Avacado on Toast">
+                            <p>Sliced avocado on whole grain toast with a soft boiled egg on the side.</p>
+                        </div>
+                    </figure>
+                </div>
+            </div>
+
+            <div class="wp-block-columns has-2-columns">
+                <div class="wp-block-column">
+                    <figure class="wp-block-dining-dashboard-menu-item">
+                        <header>
+                            <h3>Breakfast Waffles</h3>
+                            <span class="price">$12.00</span>
+                        </header>
+                        <div class="description">
+                            <img src="/wp-content/uploads/2019/02/taylor-kiser-373466-unsplash.jpg">
+                            <p>Fresh waffles with spiced caramel cream, pistachios and walnuts.</p>
+                        </div>
+                    </figure>
+                </div>
+                <div class="wp-block-column">
+                    <figure class="wp-block-dining-dashboard-menu-item">
+                        <header>
+                            <h3>French Toast</h3>
+                            <span class="price">$14.00</span>
+                        </header>
+                        <div class="description">
+                            <img src="/wp-content/uploads/2019/02/toa-heftiba-243303-unsplash.jpg">
+                            <p>Delicious French toast with strawberries, blueberries and a splash of cream.</p>
+                        </div>
+                    </figure>
+                </div>
+            </div>
+            <div class="wp-block-columns has-2-columns">
+                <div class="wp-block-column">
+                    <figure class="wp-block-dining-dashboard-menu-item">
+                        <header>
+                            <h3>Kiwi Breakfast</h3>
+                            <span class="price">$16.00</span>
+                        </header>
+                        <div class="description">
+                            <img src="/wp-content/uploads/2019/02/andy-wang-658871-unsplash-1.jpg">
+                            <p>Honey cured bacon, fried egg, sausage, baked beans, toast, grilled tomatoes and mushrooms.</p>
+                        </div>
+                    </figure>
+                </div>
+                <div class="wp-block-column">
+                    <figure class="wp-block-dining-dashboard-menu-item">
+                        <header>
+                            <h3>Fruit Platter</h3>
+                            <span class="price">$16.00</span>
+                        </header>
+                        <div class="description">
+                            <img src="/wp-content/uploads/2019/02/heather-ford-731018-unsplash.jpg">
+                            <p>Seasonal fruit served with cinnamon toast, crackers and creamy Greek yogurt.</p>
+                        </div>
+                    </figure>
+                </div>
+            </div>
+        </div><!-- .entry-content -->
+    </article>
+
+</div>
